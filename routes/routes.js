3c374c8b66e3f2198d8647b4a7bc8f203f34cc84
twei <twei@solidreturn.co.uk>
2014-10-16 00:31:06 +0100

    add new routes.js as central control

---
+++ routes/routes.js
@@ -0,0 +1,45 @@
+﻿module.exports = function (app, passport) {
+    app.get('/', function (req, res) {
+        console.log('The magic happens on page ' + res);
+        res.render('index');
+    });
+    
+    // PROFILE SECTION =========================
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user : req.user
+        });
+    });
+    
+ 
+    
+    // LOGIN ===============================
+    // show the login form
+    app.get('/login', function (req, res) {
+        res.render('login', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+   app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+    app.get('/signup', function (req, res) {
+        
+    });
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+}
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    
+    res.redirect('/');
+}
