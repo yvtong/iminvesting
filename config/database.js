d0cfcc35b78d0f5a632a5ed31f9f10f34edcdce1
twei <twei@solidreturn.co.uk>
2014-10-16 00:17:49 +0100

    add configuration for mongodb and login

---
+++ config/database.js
@@ -0,0 +1,5 @@
+﻿module.exports = {
+    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    //'url': 'mongodb://developer:passw0rd@localhost:27017/solidreturn'
+    'url': 'mongodb://developer:passw0rd@ds041160.mongolab.com:41160/solidreturn'
+};
